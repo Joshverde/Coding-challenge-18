67e6cba297b5b3196446e179fd6273e4d1709283
Joshverde <joshverde@verizon.net>
2025-04-04 18:25:55 -0400

    code improvements

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -2,7 +2,7 @@
 function Greeting (){
     return <div>
     <h2>Hello from the Greeting Component!</h2>
-    <p>This is your first custom component in React.</p>
+    <p>This is your first custom component in Reacts.</p>
   </div>;
 }
 
